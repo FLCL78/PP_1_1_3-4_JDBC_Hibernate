e05b3f79e6608ab23287d2df4891ce40ad43f94a
FLCL <Dombovoz@gmail.com>
2025-04-07 00:40:54 +0300

    HibernateBranch v 1.02

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,6 +4,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 
 public class Main {
@@ -22,6 +23,7 @@ public class Main {
         }
         userService.cleanUsersTable();
         userService.dropUsersTable();
+        Util.shutdown();
 
 
 
